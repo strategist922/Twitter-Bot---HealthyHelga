7046b082ca6aade53cea024f90fcd52cd0dee1c5
Hassan Nasif <hrnasif@gmail.com>
2016-08-17 12:12:17 +0200

    Updated script

--- Script.R
+++ Script.R
@@ -6,10 +6,10 @@ library(dplyr)
 
 #Getting the twitter authorization setup for HealthyHelga
 
-consumer_key = 'mbMi4KCyZxTbfvrQ4GTK2OWkV'
-consumer_secret = 'pfx4VWrWyeEkVDvS4Hub1CEfoxl6VidSb4fjw4H7iiwrf2x3cJ'
-access_token = '751358493723099137-4Y9WOo8pVvqTiKCVj86PxYO31XPkBph' 
-access_secret = 'ijZCBRPYLe56EJLzbBQ7Sqt6aeBCiIZgj5M7XA5iKWsji'
+consumer_key = ''
+consumer_secret = ''
+access_token = '' 
+access_secret = ''
 
 setup_twitter_oauth(consumer_key, consumer_secret, access_token, access_secret)
 
