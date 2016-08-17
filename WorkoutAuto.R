12bedb6ee518f81c526f1b4b36947063101b6021
Hassan Nasif <hrnasif@gmail.com>
2016-08-17 13:26:18 +0200

    Updated script

--- WorkoutAuto.R
+++ WorkoutAuto.R
@@ -3,12 +3,14 @@ library(tm)
 library(stringr)
 library(httr)
 library(rvest)
-consumer_key <- "mbMi4KCyZxTbfvrQ4GTK2OWkV"
-consumer_secret <- "pfx4VWrWyeEkVDvS4Hub1CEfoxl6VidSb4fjw4H7iiwrf2x3cJ"
+
+consumer_key = ''
+consumer_secret = ''
+access_token = '' 
+access_secret = ''
+
 author <- "HealthyHelga"
 author_id <- "751358493723099137"
-access_token <- "751358493723099137-4Y9WOo8pVvqTiKCVj86PxYO31XPkBph"
-access_secret <- "ijZCBRPYLe56EJLzbBQ7Sqt6aeBCiIZgj5M7XA5iKWsji"
 setup_twitter_oauth(consumer_key, consumer_secret, access_token, access_secret)
 
 # Extract workout links
